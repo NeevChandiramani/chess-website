d23127f464e08c10b5f183d7423b9ddfecdc0a4f
NeevChandiramani <neev.chandiramani@gmail.com>
2024-04-30 18:28:18 +0200

    Update index.html and style.css with language selector and chess rules

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // JavaScript code will go here
+document.addEventListener("DOMContentLoaded", function() {
+    const englishButton = document.getElementById("english-button");
+    const frenchButton = document.getElementById("french-button");
+    const englishContent = document.getElementById("english-content");
+    const frenchContent = document.getElementById("french-content");
+    
+    englishButton.addEventListener("click", function() {
+        englishContent.style.display = "block";
+        frenchContent.style.display = "none";
+    });
+
+    frenchButton.addEventListener("click", function() {
+        englishContent.style.display = "none";
+        frenchContent.style.display = "block";
+    });
+});
